e025409a6778407a9f96a1b27aa69295e40aad8d
Gabriel Nima <gabrielnimap@gmail.com>
2024-03-03 21:04:15 -0500

    final

--- 03-mouse-follower/src/App.jsx
+++ 03-mouse-follower/src/App.jsx
@@ -1,3 +1,44 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
-export const App = () => {};
+export const App = () => {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  useEffect(() => {
+    console.log("Efecto");
+    const handleMove = () => {
+      const { clientX, clientY } = event;
+      console.log("handleMove", { clientX, clientY });
+      setPosition({ x: clientX, y: clientY });
+    };
+
+    if (enabled) {
+      window.addEventListener("pointermove", handleMove);
+    }
+    return () => {
+      window.removeEventListener("pointermove", handleMove); //getEventListeners
+    };
+  }, [enabled]);
+
+  return (
+    <main>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "#00f",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      ></div>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? "Desactivar" : "Activar"} seguir puntero
+      </button>
+    </main>
+  );
+};
